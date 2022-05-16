0b69cb2be2b97cbde3a9585e0b6f45abec0f6cec
akncnkoc <akincankoc@gmail.com>
2022-05-16 13:01:23 +0300

    github workflow addded, playwright tests and binded input

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
+import { useState } from "react"
+import { BindedInput } from "../components/core-ui/BindedInput";
 import Tab from "../components/core-ui/tab/Tab"
 
 const HomePage: React.FC = (props) => {
-  return <div className="w-full">
-    <Tab>
-      <div label="Genel Rapor">
-        test
-      </div>
-      <div label="Genel Rapor 2">
-        test 2
-      </div>
-    </Tab>
-  </div>
+  const [form, setForm] = useState({
+    name: ""
+  });
+  return (
+    <div className="w-full">
+      <Tab>
+        <div label="Genel Rapor">
+          <BindedInput label="Ad" name="name" bindTo={setForm} />
+        </div>
+        <div label="Genel Rapor 2">test 2</div>
+      </Tab>
+    </div>
+  )
 }
 
 export default HomePage
