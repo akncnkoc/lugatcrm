9fee018f80c6307c30595d899f2e6d02a83e572a
akncnkoc <akincankoc@gmail.com>
2022-05-16 22:33:01 +0300

    grid updated to styled component

--- components/core-ui/Miscellaneous.tsx
+++ components/core-ui/Miscellaneous.tsx
@@ -1,20 +1,12 @@
-export const Grid: React.FC<
-  {
-    row?: number
-    col?: number
-    className?: string
-  } & React.HtmlHTMLAttributes<HTMLDivElement>
-> = (props) => {
-  const { row = 1, col = 1, className, ...args } = props
-  return (
-    <div
-      className={`grid ${className}`}
-      style={{
-        gridTemplateRows: `repeat(${row}, minmax(0, 1fr))`,
-        gridTemplateColumns: `repeat(${col}, minmax(0, 1fr))`,
-      }}
-      {...args}>
-      {props.children}
-    </div>
-  )
-}
+import styled from 'styled-components'
+
+type GridProps = {
+  row?: number,
+  column?: number
+} & React.HtmlHTMLAttributes<HTMLDivElement>
+
+export const Grid = styled.div<GridProps>`
+  display: grid;
+  grid-template-rows: repeat(${(props) => props.row}, minmax(0,1fr));
+  grid-template-columns: repeat(${(props) => props.column}, minmax(0,1fr));
+`;
