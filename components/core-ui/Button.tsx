12f11bd5f1dfee515358e81aded054446fd25148
akncnkoc <akincankoc@gmail.com>
2022-05-17 15:17:40 +0300

    button updated and added globalstyles from styled-components

--- components/core-ui/Button.tsx
+++ components/core-ui/Button.tsx
@@ -1,57 +1,92 @@
 import React from "react"
 import Link from "next/link"
+import styled from "styled-components"
 import { VscLoading } from "react-icons/vsc"
 
 type ButtonProps = {
-  children?: string | JSX.Element | JSX.Element[] | undefined | any
   bgColor?: string
-  className?: string
-  disabled?: boolean
   hidden?: boolean
   ref?: string | undefined
   icon?: string | React.ReactNode | React.ReactNode[]
   loading?: boolean
-} & React.DetailedHTMLProps<
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->
+}
+
+const ButtonStyled = styled.button<ButtonProps>`
+  position: relative;
+  border-radius: 6px;
+  background-color: rgb(79 70 229 / 1);
+  padding: 10px 16px;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 500;
+  color: white;
+  &:hover {
+    background-color: rgb(55 48 163 / 1);
+  }
+  &:focus {
+    outline: none;
+  }
+  &:disabled {
+    pointer-events: none;
+    opacity: 50%;
+  }
+  display: ${(props) => props.icon && "flex"};
+  align-items: ${(props) => props.icon && "center"};
+  visibility: ${(props) => props.hidden && "hidden"};
+`
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  const {
-    children,
-    bgColor,
-    className,
-    disabled = false,
-    hidden = false,
-    loading = false,
-    ref,
-    icon,
-    ...args
-  } = props
-
-  const buttonRef = React.createRef<HTMLButtonElement>()
   return (
-    <button
-      ref={buttonRef}
-      className={`relative rounded-md bg-indigo-700 py-2.5 px-4 text-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800
-      ${(bgColor && bgColor) || "bg-indigo-600"} text-white 
-       ${disabled && "pointer-events-none disabled:opacity-50"}
-       ${icon && "flex items-center"}
-       ${hidden && "hidden"}
-       ${className}
-      `}
-      disabled={disabled}
-      {...args}>
-      {icon && <span className={`${children && "mr-1"} block`}>{icon}</span>}
-      {loading && (
-        <div className="flex justify-center">
+    <ButtonStyled>
+      {props.loading && (
+        <div
+          style={{
+            display: "flex",
+            justifyItems: "center",
+          }}>
           <VscLoading className="animate-spin text-base" />
         </div>
       )}
-      {children && !loading && children}
-    </button>
+    </ButtonStyled>
   )
 }
+
+// export const Button: React.FC<ButtonProps> = (props) => {
+//   const {
+//     children,
+//     bgColor,
+//     className,
+//     disabled = false,
+//     hidden = false,
+//     loading = false,
+//     ref,
+//     icon,
+//     ...args
+//   } = props
+
+//   const buttonRef = React.createRef<HTMLButtonElement>()
+//   return (
+//     <button
+//       ref={buttonRef}
+//       className={`relative rounded-md bg-indigo-700 py-2.5 px-4 text-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800
+//       ${(bgColor && bgColor) || "bg-indigo-600"} text-white
+//        ${disabled && "pointer-events-none disabled:opacity-50"}
+//        ${icon && "flex items-center"}
+//        ${hidden && "hidden"}
+//        ${className}
+//       `}
+//       disabled={disabled}
+//       {...args}>
+//       {icon && <span className={`${children && "mr-1"} block`}>{icon}</span>}
+//       {loading && (
+//         <div className="flex justify-center">
+//           <VscLoading className="animate-spin text-base" />
+//         </div>
+//       )}
+//       {children && !loading && children}
+//     </button>
+//   )
+// }
 export const LinkButton: React.FC<{ to: string } & ButtonProps> = (props) => {
   const { to, children, ...args } = props
   return (
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,43 @@
 import Router from "next/router"
 import NProgress from "nprogress"
 import "nprogress/nprogress.css"
-import { useEffect } from 'react'
+import { useEffect } from "react"
 import Layout from "../components/Layout"
 import { ModalProvider } from "../context/modal-context"
 import { ConfirmContextProvider } from "../modals/global/useConfirm"
 import "../styles/globals.css"
+
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  .animate-spin {
+    animation: spin 1s linear infinite;
+  }
+`
 
 Router.events.on("routeChangeStart", () => NProgress.start())
 Router.events.on("routeChangeComplete", () => NProgress.done())
 Router.events.on("routeChangeError", () => NProgress.done())
 
 export default function App({ Component, pageProps }) {
-
   useEffect(() => {
-      const { fetch: originalFetch } = window
-      window.fetch = async (...args) => {
-        NProgress.start()
-        let [resource, config] = args
-        const response = await originalFetch(resource, config)
-        NProgress.done()
-        return response
-      }
-  }, []);
+    const { fetch: originalFetch } = window
+    window.fetch = async (...args) => {
+      NProgress.start()
+      let [resource, config] = args
+      const response = await originalFetch(resource, config)
+      NProgress.done()
+      return response
+    }
+  }, [])
   return (
     <ModalProvider>
+      <GlobalStyles />
       <ConfirmContextProvider>
         <Layout>
           <Component {...pageProps} />
