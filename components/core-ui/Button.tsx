f11cc86e8c7eaa88cb607e94a391e06edbecf670
akncnkoc <akincankoc@gmail.com>
2022-05-18 22:25:33 +0300

    remove unncessarcy comments in button

--- components/core-ui/Button.tsx
+++ components/core-ui/Button.tsx
@@ -64,43 +64,6 @@ export const Button: React.FC<ButtonProps> = (props) => {
     </ButtonStyled>
   )
 }
-
-// export const Button: React.FC<ButtonProps> = (props) => {
-//   const {
-//     children,
-//     bgColor,
-//     className,
-//     disabled = false,
-//     hidden = false,
-//     loading = false,
-//     ref,
-//     icon,
-//     ...args
-//   } = props
-
-//   const buttonRef = React.createRef<HTMLButtonElement>()
-//   return (
-//     <button
-//       ref={buttonRef}
-//       className={`relative rounded-md bg-indigo-700 py-2.5 px-4 text-center text-sm font-medium text-white hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800
-//       ${(bgColor && bgColor) || "bg-indigo-600"} text-white
-//        ${disabled && "pointer-events-none disabled:opacity-50"}
-//        ${icon && "flex items-center"}
-//        ${hidden && "hidden"}
-//        ${className}
-//       `}
-//       disabled={disabled}
-//       {...args}>
-//       {icon && <span className={`${children && "mr-1"} block`}>{icon}</span>}
-//       {loading && (
-//         <div className="flex justify-center">
-//           <VscLoading className="animate-spin text-base" />
-//         </div>
-//       )}
-//       {children && !loading && children}
-//     </button>
-//   )
-// }
 export const LinkButton: React.FC<{ to: string } & ButtonProps> = (props) => {
   const { to, children, ...args } = props
   return (
